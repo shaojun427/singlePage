d8b9a8582b83b7816f1919cd73690844389aba86
williamZhou <williamzhou@williamZhoudeMacBook-Pro.local>
2017-11-05 20:15:24 +0800

    test

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const webpack = require("webpack");
+const EX = require("extract-text-webpack-plugin");//为了单独打包css
+const EXCSS = new EX('[name].css');
+module.exports = {
+  entry: {
+    'index': './src/app/app.js',
+    'zh-CN': './src/i18n/zh-CN.js',
+    'en': './src/i18n/en.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: 8080
+  },
+  module:{
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: path.resolve(__dirname, 'node_modules'),
+      include:[
+        path.resolve(__dirname, 'src/app'),
+        path.resolve(__dirname, 'src/pages')
+      ],
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-3'],
+            plugins: ['transform-runtime']
+          }
+        }
+      ]
+    },
+    {
+      test: /\.(png|jpg|gif)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      ]
+    }, {
+      test: /\.(less|css)$/,
+      use: EXCSS.extract({
+        fallback: "style-loader",
+        use: ['css-loader', 'less-loader']
+      })
+    }]
+  },
+  plugins:[
+    EXCSS,
+    new webpack.DefinePlugin({
+        locale:JSON.stringify('[name].css')
+    })
+  ]
+};
